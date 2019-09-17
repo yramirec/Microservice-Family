7435f34f6e2a875fd38b73ee70e6952fba00797c
Yordan Yosef Ramirez Calzado <yramirec@everis.com>
2019-09-17 18:14:57 -0500

    Conection-Config-Server

---
+++ src/test/java/com/everis/controller/ParentControllerTest.java
@@ -0,0 +1,14 @@
+package com.everis.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ParentControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/everis/service/ParentServiceTest.java
@@ -0,0 +1,5 @@
+package com.everis.service;
+
+public class ParentServiceTest {
+
+}
